457879e07261db92c4fc7d2b1895ea054c0a56eb
TygoDev <tygohaan@gmail.com>
2023-09-27 15:10:34 +0200

    messy furnace functionality.. needs refactor BADLY

--- Reach for the Mines/Assets/_Scripts/Canvasses/FurnaceCanvas.cs
+++ Reach for the Mines/Assets/_Scripts/Canvasses/FurnaceCanvas.cs
@@ -7,17 +7,18 @@ using System.Linq;
 
 public class FurnaceCanvas : MonoBehaviour
 {
+    [SerializeField] private List<Smeltable> smeltables = new List<Smeltable>();
     [SerializeField] private List<InventoryItem> inventoryItems = new List<InventoryItem>();
 
     [SerializeField] private InventoryItem fuelSlot = null;
     [SerializeField] private InventoryItem inputSlot = null;
     [SerializeField] private InventoryItem outputSlot = null;
-    [SerializeField] private InventoryItem slotToFill = null;
-
     [SerializeField] private Image furnaceMenu = null;
     [SerializeField] private Image inventoryMenu = null;
 
     private Systems systems = default;
+    private InventoryItem slotToFill = null;
+    private bool isProcessing = false;
 
     private void Start()
     {
@@ -62,6 +63,7 @@ public class FurnaceCanvas : MonoBehaviour
         }
         else
         {
+            isProcessing = false;
             TakeOutput(inventoryItem);
         }
 
@@ -71,13 +73,14 @@ public class FurnaceCanvas : MonoBehaviour
     {
         if (!inventoryItem.empty && slotToFill.empty)
         {
-            if(inventoryItem.item.itemType == ItemType.Smeltable && slotToFill == inputSlot)
+            if (inventoryItem.item.itemType == ItemType.Smeltable && slotToFill == inputSlot)
             {
                 SwapMenu();
                 inputSlot.FillSlot(inventoryItem.itemStack);
                 systems.inventoryManager.Remove(inventoryItem.itemStack);
                 PopulateInventory();
                 slotToFill = null;
+                StartCoroutine(Smelt());
             }
             else if (inventoryItem.item.itemType == ItemType.Fuel && slotToFill == fuelSlot)
             {
@@ -86,6 +89,51 @@ public class FurnaceCanvas : MonoBehaviour
                 systems.inventoryManager.Remove(inventoryItem.itemStack);
                 PopulateInventory();
                 slotToFill = null;
+                StartCoroutine(Smelt());
+            }
+        }
+    }
+
+    public void Process()
+    {
+        if (!isProcessing)
+            return;
+
+        foreach (Smeltable smeltable in smeltables)
+        {
+            if (smeltable.itemInput == inputSlot.item)
+            {
+                if (outputSlot.empty)
+                {
+                    outputSlot.FillSlot(new ItemStack(smeltable.itemOutput, 1));
+                }
+                else if (outputSlot.item == smeltable.itemOutput)
+                {
+                    ItemStack newStackOutput = outputSlot.itemStack;
+                    newStackOutput.quantity++;
+                    outputSlot.ClearSlot();
+                    outputSlot.FillSlot(newStackOutput);
+                }
+                else
+                {
+                    return;
+                }
+
+
+                ItemStack newStackInput = inputSlot.itemStack;
+                newStackInput.quantity -= 5;
+                inputSlot.ClearSlot();
+
+                if (newStackInput.quantity != 0)
+                    inputSlot.FillSlot(newStackInput);
+                
+
+                ItemStack newStackFuel = fuelSlot.itemStack;
+                newStackFuel.quantity--;
+                fuelSlot.ClearSlot();
+
+                if (newStackFuel.quantity != 0)
+                    fuelSlot.FillSlot(newStackFuel);                 
             }
         }
     }
@@ -113,6 +161,19 @@ public class FurnaceCanvas : MonoBehaviour
         }
     }
 
+    private IEnumerator Smelt()
+    {
+        if (!inputSlot.empty && !fuelSlot.empty &&
+        inputSlot.itemStack.quantity >= 5 && fuelSlot.itemStack.quantity >= 1 &&
+        !isProcessing)
+        {
+            isProcessing = true;
+            yield return new WaitForSeconds(2);
+            Process();
+            isProcessing = false;
+            StartCoroutine(Smelt());
+        }
+    }
 
     // EVENT LISTENERS
 
@@ -130,3 +191,16 @@ public class FurnaceCanvas : MonoBehaviour
         }
     }
 }
+
+[System.Serializable]
+public class Smeltable
+{
+    public Item itemInput;
+    public Item itemOutput;
+    public Smeltable(Item pitemInput, Item pitemOutput)
+    {
+        this.itemInput = pitemInput;
+        this.itemOutput = pitemOutput;
+    }
+}
+
