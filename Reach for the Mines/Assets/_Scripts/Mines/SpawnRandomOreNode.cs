2c7de11631025b484f68289d67f24aa3044b8d31
TygoDev <tygohaan@gmail.com>
2023-10-31 15:49:37 +0100

    lighting fix as well as copper cave scene switching

--- Reach for the Mines/Assets/_Scripts/Mines/SpawnRandomOreNode.cs
+++ Reach for the Mines/Assets/_Scripts/Mines/SpawnRandomOreNode.cs
@@ -12,4 +12,10 @@ public class SpawnRandomOreNode : MonoBehaviour
 
         Instantiate(oreNodes[random], transform);
     }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawCube(transform.position, new Vector3(1,1,1));
+    }
 }
