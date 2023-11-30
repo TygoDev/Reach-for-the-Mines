e52fee8c5a8552ea1382c7a2101b7b340cbc1059
TygoDev <tygohaan@gmail.com>
2023-11-30 15:03:03 +0100

    Visual timer + time reduction

--- Reach for the Mines/Assets/_Scripts/Systems/CaveManager.cs
+++ Reach for the Mines/Assets/_Scripts/Systems/CaveManager.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,6 +10,7 @@ public class CaveManager : MonoBehaviour
 
     [SerializeField] private float timeInCave = 5;
     [SerializeField] private Vector3 spawnpoint = new Vector3(0,1,0);
+    [SerializeField] private TMP_Text timerText = null;
 
     private Systems systems;
     private Timer caveTime = null;
@@ -29,6 +32,13 @@ public class CaveManager : MonoBehaviour
     private void Update()
     {
         caveTime.Tick(Time.deltaTime);
+
+        int minutes = Mathf.FloorToInt(caveTime.remainingSeconds / 60F);
+        int seconds = Mathf.FloorToInt(caveTime.remainingSeconds - minutes * 60);
+
+        string niceTime = string.Format("{0:0}:{1:00}", minutes, seconds);
+
+        timerText.text = $"Time Left:\n{niceTime}";
     }
 
     void TimerEnd()
