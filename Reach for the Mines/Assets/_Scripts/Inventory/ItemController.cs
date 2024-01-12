66b96a33e8e6a340844ddf16bf33365b048f799c
TygoDev <tygohaan@gmail.com>
2024-01-12 10:48:20 +0100

    magnetizing drops towards player

--- Reach for the Mines/Assets/_Scripts/Inventory/ItemController.cs
+++ Reach for the Mines/Assets/_Scripts/Inventory/ItemController.cs
@@ -5,23 +5,19 @@ using UnityEngine;
 public class ItemController : MonoBehaviour
 {
     public Item item = null;
-
-    private Systems systems = default;
     private const string PLAYER = "Player";
+    GameObject target = null;
+    private float attractionForce = 10f;
+    private float maxAttractionRange = 5f;
 
     private void Start()
     {
-        systems = Systems.Instance;
-    }
-
-    private void Awake()
-    {
-        systems = Systems.Instance;
+        target = GameObject.FindGameObjectWithTag(PLAYER);
     }
 
     private void PickUp()
     {
-        systems.inventoryManager.Add(item, gameObject);
+        Systems.Instance.inventoryManager.Add(item, gameObject);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -29,4 +25,18 @@ public class ItemController : MonoBehaviour
         if (other.gameObject.CompareTag(PLAYER))
             PickUp();
     }
+
+    private void Update()
+    {
+        Vector3 directionToTarget = target.transform.position - transform.position;
+        float distanceToTarget = directionToTarget.magnitude;
+
+        if (distanceToTarget > 0 && distanceToTarget <= maxAttractionRange)
+        {
+            float force = Mathf.Clamp(1 / distanceToTarget, 0f, 1f) * attractionForce;
+            Vector3 normalizedDirection = directionToTarget.normalized;
+            transform.Translate(normalizedDirection * force * Time.deltaTime, Space.World);
+        }
+    }
+
 }
